f280f9ef9acf98ce5584aef7917d5dfd5b100161
Ignacio Buioli <ibuioli@gmail.com>
2020-09-17 00:09:46 -0300

    feat: version controller

---
+++ __manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "odoo_api",
+
+    'summary': """
+        Odoo API XMLRPC Style""",
+
+    'description': """
+        Odoo API XMLRPC Style
+    """,
+
+    'author': "Moldeo Interactive",
+    'website': "https://www.moldeointeractive.com.ar",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [],
+    # only loaded in demonstration mode
+    'demo': [],
+}
---
+++ controllers/controllers.py
@@ -0,0 +1,21 @@
+# API Connector
+# XMLRPC Style for Odoo
+# Main Dev: Ignacio Buioli <ibuioli@gmail.com> or <ignacio.buioli@moldeointeractive.com.ar>
+# Moldeo Interactive - https://www.moldeointeractive.com.ar
+
+# -*- coding: utf-8 -*-
+from odoo import http
+from odoo.http import request
+import re
+
+class OdooApiXMLRPC(http.Controller):
+    @http.route('/odoo-api/common/login', type="json", auth='none', cors='*')
+    def odoo_api_login(self, **kw):
+        model = request.env['ir.module.module'].sudo().search([('name', '=', 'base')], limit=1)
+        version = model.installed_version.split('.')
+        return {
+            "server_version": version[0] + ".0",
+            "server_version_info": [int(version[0]), 0, 0, "final", 0],
+            "server_serie": version[0] + ".0",
+            "protocol_version": 1,
+        }
